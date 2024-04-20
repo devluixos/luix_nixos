1dc74756946f3bbcb277b58ec4c162966eb15df3
Luiz P <luiizperren@hotmail.com>
2024-04-20 23:39:43 +0200

    Rebuild at generation:

--- rebuild-and-commit.sh
+++ rebuild-and-commit.sh
@@ -15,19 +15,17 @@ pushd $DOTFILES_DIR  # Change to the correct directory containing flake.nix
 echo "Formatting Nix files..."
 alejandra . &>> $DOTFILES_DIR/nixos-switch.log
 
-echo "Adding all changes to Git..."
-git add -A
-
 echo "Checking for changes..."
-if git diff --cached --quiet; then
+if git diff --quiet; then
   echo "No changes detected."
 else
   echo "Rebuilding NixOS..."
   # Rebuild using sudo, specifically for nixos-rebuild which needs root privileges
-  if sudo nixos-rebuild switch --flake $DOTFILES_DIR &>> $DOTFILES_DIR/nixos-switch.log; then
+  if sudo nixos-rebuild switch --flake $DOTFILES_DIR | tee -a $DOTFILES_DIR/nixos-switch.log; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Rebuild successful." >> $DOTFILES_DIR/nixos-switch.log
     echo "Committing changes..."
     gen=$(sudo nixos-rebuild list-generations | grep current | cut -d' ' -f2-)
+    git add -A
     git commit -m "Rebuild at generation: $gen"
     git push
     echo "Commit successful. Generation: $gen."
