a88c15918f0392bca820a4fea752fb2cbedccc2b
Luiz P <luiizperren@hotmail.com>
2024-04-25 22:02:26 +0200

    Rebuild at generation:

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -137,6 +137,11 @@
     '';
   };
 
+  hardware.opengl.extraPackages = with pkgs; [
+    rocm-opencl-icd
+    rocm-runtime
+  ];
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
